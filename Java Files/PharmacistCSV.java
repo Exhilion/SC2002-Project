d2427a6f2082136f12969a67d5dedccfcf147b1f
dma006 <dma006@e.ntu.edu.sg>
2024-11-13 23:46:11 +0800

    edit

--- Java Files/PharmacistCSV.java
+++ Java Files/PharmacistCSV.java
@@ -8,22 +8,20 @@ import java.util.List;
 
 public class PharmacistCSV {
 
-    // Method to load pharmacists from CSV
     public List<Pharmacist> loadPharmacistsFromCSV() {
         List<Pharmacist> pharmacists = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(AppConfig.PHARMACIST_FILE_PATH))) {
             String line;
-            br.readLine(); // Skip header line
+            br.readLine(); 
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                // Assuming CSV structure: HospitalID, Password, Role, Gender, Name, firstTimeLogin
                 Pharmacist pharmacist = new Pharmacist(
-                    values[0], // HospitalID
-                    values[1], // Password
-                    Role.PHARMACIST, // Hardcoded role since it's a pharmacist
-                    Gender.valueOf(values[3].toUpperCase()), // Gender
-                    values[4], // Name
-                    Boolean.parseBoolean(values[5]) // firstTimeLogin
+                    values[0],
+                    values[1],
+                    Role.PHARMACIST, 
+                    Gender.valueOf(values[3].toUpperCase()), 
+                    values[4],
+                    Boolean.parseBoolean(values[5]) 
                 );
                 pharmacists.add(pharmacist);
             }
