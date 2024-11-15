5c2b536aa5ee487e4ecbf10ff386d95d9ba5209b
Shaun Cheong <56810865+Kouhai-Neko@users.noreply.github.com>
2024-11-15 21:57:38 +0800

    Delete PatientMedRecords.java

--- PatientMedRecords.java
+++
@@ -1,6 +0,0 @@
-package OOPProject;
-
-public interface PatientMedRecords {
-	MedicalRecord viewMedicalRecords(String PatientID);
-	void updateMedicalRecords(String PatientID, MedicalRecord records);
-}
