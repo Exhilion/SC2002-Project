fc47d34cd6073cea166bfc8a104398968a589a6b
dma006 <dma006@e.ntu.edu.sg>
2024-11-15 09:17:18 +0800

    pharm edit

--- Java Files/Pharmacist.java
+++ Java Files/Pharmacist.java
@@ -2,18 +2,35 @@ package OOPProject;
 
 public class Pharmacist extends User {
 	private String pharmacistName;
+	private String hospitalID;
+	private String password;
+	private Role role; 
+	private Gender gender; 
+	
+	private static PharmacistCSV pharmacistCSV  = new PharmacistCSV();
 
-    public Pharmacist(String hospitalId, String password, Role role, Gender gender, 
+    public Pharmacist(String hospitalID, String password, Role role, Gender gender, 
                       String pharmacistName, Boolean firstTimeLogin) {
-        super(hospitalId, password, role, gender, firstTimeLogin);
+        super(hospitalID, password, role, gender, firstTimeLogin);
         this.pharmacistName = pharmacistName;
+        this.hospitalID = hospitalID; 
+        this.role = role;
+        this.gender = gender;
     }
 
     public String getPharmacistName() {
         return pharmacistName;
     }
-    public Pharmacist() {
-    	
+    
+    public String getHospitalID() {
+        return hospitalID;
     }
-
+    
+    public Role getrole() {
+        return role;
+    }
+    
+    public Gender getGender() {
+		return gender; 
+    }
 }
