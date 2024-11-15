eba28308a6f8de2b7c58ab5bbe487d45ff4c4017
Shaun Cheong <56810865+Kouhai-Neko@users.noreply.github.com>
2024-11-15 21:56:25 +0800

    Delete UserAuthentication.java

--- UserAuthentication.java
+++
@@ -1,8 +0,0 @@
-package OOPProject;
-
-public interface UserAuthentication {
-	boolean login(String hospitalID, String password);
-    void changePassword(String hospitalID, String oldPassword, String newPassword);
-    void forgotPassword(String hospitalID);
-    boolean logOut(String hospitalID);
-}
