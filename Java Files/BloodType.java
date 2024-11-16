f2fd5f43e9ce388e17dcae5cb59fba6c00b1ef0d
dma006 <dma006@e.ntu.edu.sg>
2024-11-17 00:05:43 +0800

    Added Javadoc implementation

--- Java Files/BloodType.java
+++ Java Files/BloodType.java
@@ -1,5 +1,49 @@
 package OOPProject;
 
+/**
+ * Represents the different blood types in the ABO and Rh blood group systems.
+ * This enum includes positive and negative types for each of the major groups.
+ * 
+ * <p>
+ * The available blood types are:
+ * <ul>
+ *   <li>O Positive (OPOS)</li>
+ *   <li>O Negative (ONEG)</li>
+ *   <li>A Positive (APOS)</li>
+ *   <li>A Negative (ANEG)</li>
+ *   <li>AB Positive (ABPOS)</li>
+ *   <li>AB Negative (APNEG)</li>
+ *   <li>B Positive (BPOS)</li>
+ *   <li>B Negative (BNEG)</li>
+ * </ul>
+ * </p>
+ * 
+ * <p>
+ * This enum is used for managing blood type-related information.
+ * </p>
+ */
 public enum BloodType {
-	OPOS, ONEG, APOS, ANEG, ABPOS, APNEG, BPOS, BNEG 
+    /** O Positive blood type. */
+    OPOS, 
+    
+    /** O Negative blood type. */
+    ONEG, 
+    
+    /** A Positive blood type. */
+    APOS, 
+    
+    /** A Negative blood type. */
+    ANEG, 
+    
+    /** AB Positive blood type. */
+    ABPOS, 
+    
+    /** AB Negative blood type. */
+    APNEG, 
+    
+    /** B Positive blood type. */
+    BPOS, 
+    
+    /** B Negative blood type. */
+    BNEG 
 }
