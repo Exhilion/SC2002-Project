731e9b34976064dd5cf124219c5332658a36c9d1
Chester Sam <164471394+Exhilion@users.noreply.github.com>
2024-11-17 01:10:50 +0800

    Added Javadoc implementation

--- Java Files/Patient.java
+++ Java Files/Patient.java
@@ -12,6 +12,12 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * The Patient class represents a patient in the hospital system.
+ * It extends the User class and contains personal details such as name, date of birth, gender, blood type, 
+ * hospital ID, password, contact information, and role.
+ * The class also includes methods for retrieving and updating patient records.
+ */
 public class Patient extends User{
 	private String name; 
 	private Date dateOfBirth; 
@@ -23,11 +29,20 @@ public class Patient extends User{
 	private String phoneNumber; 
 	private String email; 
 
-
-	
-	
-	
-	
+	/**
+     * Constructs a new Patient object with the specified attributes.
+     * 
+     * @param hospitalID The patient's unique hospital ID.
+     * @param password The patient's password.
+     * @param role The patient's role (e.g., patient).
+     * @param gender The patient's gender.
+     * @param name The patient's full name.
+     * @param dateOfBirth The patient's date of birth.
+     * @param bloodType The patient's blood type.
+     * @param phoneNumber The patient's phone number.
+     * @param email The patient's email address.
+     * @param firstTimeLogin Indicates whether this is the patient's first time logging in.
+     */
 	public Patient(String hospitalID, String password, Role role, Gender gender, 
             String name, Date dateOfBirth, BloodType bloodType, 
             String phoneNumber, String email, Boolean firstTimeLogin) {
@@ -42,40 +57,110 @@ public class Patient extends User{
 			 this.role = role; 
 	}
 
+	/**
+     * Gets the patient's hospital ID, which serves as their username.
+     * 
+     * @return The hospital ID of the patient.
+     */
 	public String getUsername() {
 		return hospitalID; 
 	}
+	
+	/**
+     * Gets the patient's email address.
+     * 
+     * @return The email address of the patient.
+     */
 	public String getEmail() {
 		return email; 
 	}
+	
+	/**
+     * Gets the patient's role.
+     * 
+     * @return The role of the patient.
+     */
 	public Role getRole() {
 		return role;
 	}
 	
+	/**
+     * Gets the patient's full name.
+     * 
+     * @return The name of the patient.
+     */
 	public String getName() {
 		return name; 
 	}
+	
+	/**
+     * Sets the patient's name.
+     * 
+     * @param name The new name of the patient.
+     */
 	public void setName(String name) {
 		this.name = name; 
 	}
+	
+	/**
+     * Gets the patient's date of birth.
+     * 
+     * @return The date of birth of the patient.
+     */
 	public Date getDateOfBirth() {
 		return dateOfBirth; 
 	}
+	
+	/**
+     * Sets the patient's date of birth.
+     * 
+     * @param dateOfBirth The new date of birth of the patient.
+     */
 	public void setDate(Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
+	
+	/**
+     * Gets the patient's gender.
+     * 
+     * @return The gender of the patient.
+     */
 	public Gender getGender() {
 		return gender; 
 	}
+	
+	/**
+     * Sets the patient's gender.
+     * 
+     * @param gender The new gender of the patient.
+     */
 	public void setGender(Gender gender) {
 		this.gender = gender;
 	}
+	
+	/**
+     * Gets the patient's blood type.
+     * 
+     * @return The blood type of the patient.
+     */
 	public BloodType getBloodType() {
 		return bloodType;
 	}
+	
+	/**
+     * Sets the patient's blood type.
+     * 
+     * @param bloodType The new blood type of the patient.
+     */
 	public void setBloodType(BloodType bloodType) {
 		this.bloodType = bloodType; 
 	}
+	
+	/**
+     * Gets the patient's phone number.
+     * 
+     * @return The phone number of the patient.
+     */
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
