6dfeab33b4ce9979af2be0361faa5c6fa330ca8a
Chester Sam <164471394+Exhilion@users.noreply.github.com>
2024-11-17 01:32:30 +0800

    Added Javadoc implementation

--- Java Files/TreatmentCSV.java
+++ Java Files/TreatmentCSV.java
@@ -5,8 +5,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The TreatmentCSV class handles the loading of treatment data from a CSV file.
+ * It reads the CSV file and creates Treatment objects from the data.
+ */
 public class TreatmentCSV {
 	// Method to load treatments from CSV
+	/**
+     * Loads treatments from a CSV file and returns a list of Treatment objects.
+     * 
+     * @return A list of Treatment objects loaded from the CSV file.
+     */
 	 public List<Treatment> loadTreatmentsFromCSV() {
 	     List<Treatment> treatments = new ArrayList<>();
 	     try (BufferedReader br = new BufferedReader(new FileReader(AppConfig.TREATMENT_FILE_PATH))) {
