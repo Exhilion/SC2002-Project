b466740da30cf0496a93d6ade2c653e07d350f74
Chester Sam <164471394+Exhilion@users.noreply.github.com>
2024-11-17 00:58:31 +0800

    Added Javadoc implementation

--- Java Files/MedicalRecordService.java
+++ Java Files/MedicalRecordService.java
@@ -2,7 +2,21 @@ package OOPProject;
 
 import java.util.List;
 
+/**
+ * The MedicalRecordService interface defines the requirements for services related to managing medical records.
+ * Any class that implements this interface will need to provide the implementation of adding a medical record.
+ */
 public interface MedicalRecordService {
+	
+	/**
+     * Adds a new medical record with the given details.
+     * 
+     * @param recordID The unique identifier for the medical record.
+     * @param patient The patient associated with the medical record.
+     * @param diagnosis The diagnosis associated with the medical record.
+     * @param treatment The treatment associated with the medical record.
+     * @param prescriptions The list of prescriptions associated with the medical record.
+     */
     void addMedicalRecord(String recordID, Patient patient, Diagnosis diagnosis, Treatment treatment,
                           List<Prescription> prescriptions);
 }
